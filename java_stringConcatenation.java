93196315ae7f7e059a69885837cfb6a3c58dd046
emmanuelotech <150592072+emmanuelotech@users.noreply.github.com>
2025-06-07 22:15:35 +0300

    Create java_stringConcatenation.java
    String concatenation:
    Java + operator can be used to combine two or more strings.
    
    concat methond can also be used in the format.
    System.out.println(firstName + " " + lastName);

---
+++ java_stringConcatenation.java
@@ -0,0 +1,14 @@
+public class Main {
+  public static void main(String[] args) {
+    String firstName = "Jane ";
+    String lastName = "Doe";
+   
+   //string concatenation --> PowerQuerry --> Excel (Data Cleaning) DA & DS
+   
+   System.out.println(firstName + lastName +" is assigned to anonymous Female");
+   
+   //concat() method. j--. code still works.
+   System.out.println(firstName.concat(lastName).concat(" is assigned to anonymous Female"));
+   
+  }
+}
